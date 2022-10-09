e20f56ef459935aa56066b1219114a71571574f3
Marissa Bedard <mbedard95@gmail.com>
2022-10-09 16:19:22 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <HomePage/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HomePage from './components/HomePage';
+
+export default {
+  name: 'App',
+
+  components: {
+    HomePage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-app-bar app color="light-blue lighten-5" dark>
+        <div class="d-flex align-center">
+            <v-img alt="Alegeus" class="shrink mt-1 hidden-sm-and-down" contain min-width="100"
+                src="https://www.alegeus.com/wp-content/uploads/2020/06/Alegeus_Logo_RGB_Blue.svg" width="100" />
+        </div>
+
+        <v-spacer></v-spacer>
+        <div class="text-center">
+            <v-menu open-on-hover offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                        Change Requests
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item v-for="(item, index) in changeRequestItems" :key="index">
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </div>
+        <v-spacer></v-spacer>
+        <v-menu open-on-hover offset-y>
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                    Users
+                </v-btn>
+            </template>
+
+            <v-list>
+                <v-list-item v-for="(item, index) in userItems" :key="index">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <v-spacer></v-spacer>
+
+        <v-btn href="https://github.com/mbedard95/ReleaseTool" target="_blank" text color="black">
+            <span class="mr-2">Source Code</span>
+            <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+  
+<script>
+export default {
+    name: 'NavigationBar',
+
+    data: () => ({
+        changeRequestItems: [
+            { title: 'Add Change Request' },
+            { title: 'View Change Requests' },
+        ],
+        userItems: [
+            { title: 'Add User' },
+            { title: 'View Users' },
+        ],
+    }),
+}
+</script>
+  
